eeaca0cf20e2f697656632284674020a2208cc62
SirWilson <200369652@student.on.ca>
2019-09-13 15:45:42 -0400

    lesson 2 changes

---
+++ Lowballers/Controllers/StoreController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lowballers.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lowballers.Controllers
+{
+    public class StoreController : Controller
+    {
+        public IActionResult Index()
+        {
+            var categories = new List<Category>();
+
+            // create 10 mock categories
+
+            for (int i = 1; i <= 10; i++)
+            {
+                categories.Add(new Category { Name = "Category " + i.ToString() });
+            } 
+            return View(categories);
+        }
+
+        public IActionResult Browse(string category)
+        {
+            //add selected category to viewbag 
+
+            ViewBag.category = category;
+
+            return View();
+        }
+    }
+}
---
+++ Lowballers/README.md
@@ -0,0 +1,2 @@
+﻿<h1>Lowballers</h1>
+<p>This app is for COMP2048 @ LUGC in the fall of 2019 semester</p>
